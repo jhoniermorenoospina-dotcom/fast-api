d9952d695c83b58b47c9ba0e1311a5ce95bcf45a
jhoniermorenoospina-dotcom <jhoniermorenoospina@gmail.com>
2026-01-19 15:16:01 -0500

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -1,7 +1,9 @@
 from pydantic import BaseModel
 from uuid import UUID
 from datetime import datetime
+from typing import Optional
 
+# ---------- TRADE ----------
 class TradeSchema(BaseModel):
     run_id: UUID
     instrument: str
@@ -12,3 +14,12 @@ class TradeSchema(BaseModel):
     mfe: float
     mae: float
     direction: str
+
+# ---------- STRATEGY RUN ----------
+class StrategyRunCreate(BaseModel):
+    instrument: str
+    start_time: Optional[datetime] = None
+    rr: Optional[float] = None
+    be_rr: Optional[float] = None
+    trailing_rr: Optional[float] = None
+    stop_type: Optional[str] = None
